cc1cb00cd65b70658974602ba0c07cb271e4fafb
achim <achim.stein@ling.uni-stuttgart.de>
2024-11-09 20:20:15 +0100

    add coding string based on query

--- dql.py
+++ dql.py
@@ -22,6 +22,10 @@ def parse_grew_query(query_file):
         # grep coding instruction from comment line (% coding...)
         if not coding_line and re.search(r'coding', line, re.IGNORECASE):
             coding_line = line
+    """
+    coding line:   % coding attribute=modal value=verb node=MOD addlemma=V
+    adds "modal=verb(<lemma of V>)" to (1) meta information and (2) the 'misc' column of MOD node
+    """
     if coding_line:
         m = re.search(r'(attr?|attribute)=(?P<value>\w+)', coding_line, re.IGNORECASE)
         if m:
@@ -31,7 +35,10 @@ def parse_grew_query(query_file):
             coding['val'] = m.group('value')
         m = re.search(r'(node)=(?P<value>\w+)', coding_line, re.IGNORECASE)
         if m:
-            coding['node'] = m.group('value')
+            coding['node'] = m.group('value')  # adds coding string to 'misc' of this node
+        m = re.search(r'(add)?lemma=(?P<value>\w+)', coding_line, re.IGNORECASE)
+        if m:
+            coding['addlemma'] = m.group('value')  # adds lemma of this node
         print(f"Coding info: {coding}")
     else:
         print("Coding instructions not found.  Use a comment to define them, e.g. '% coding attribute=modal value=verb node=VERB'")
@@ -53,30 +60,56 @@ def match_sentences_with_query(conllu_file, query_content):
     print("Matching query against corpus...")
     match_list = corpus.search(request)
   
-    # Iterate through each graph in the corpus to apply the query.
+    # Iterate through each matching graph
     match_nr = 0 
     matched_graphs = []
     for match in match_list:
         match_nr += 1
         sent_id = match['sent_id']
         graph = corpus.get(sent_id)  # get graph for this id
-        nodes = match['matching']['nodes']
-        print(f"\n===> Match nr = {match_nr} | nodes = {nodes}\n  {match}")
         if match_nr > args.max:
             print(f"Maximum match number {args.max} reached: search stopped.")
             return matched_graphs
+        nodes = match['matching']['nodes'] # word IDs matching nodes in query
+        print(f"\n===> Match nr = {match_nr} | nodes = {nodes}\n  {match}")
         matched_graphs.append(graph)
         print(graph.to_sentence())
-        graph.json_data()["meta"]["coding"] = "my coding string"
-        graph.json_data()["meta"]["utterance"] = graph.to_sentence()
-        print(graph.to_conll())
-        print(graph.json_data())
+        j_graph = graph.json_data()  # graph in JSON format
+        print(j_graph['nodes']['1'])
+        # add coding data
+        coding = parse_grew_query(query_content)
+        j_graph["meta"]["coding"] = '' # add empty coding line
+        if coding:
+            # add coding to meta
+            addlemma_node_id = match['matching']['nodes'][coding['addlemma']] # id of coding node
+            lemma = j_graph['nodes'][addlemma_node_id]['lemma']           # lemma of this node
+            coding_string = f"{coding['att']}={coding['val']}({lemma})"
+            j_graph["meta"]["coding"] += f"{coding_string}"
+            # add coding to node specified in the coding instruction
+            coding_node_id = match['matching']['nodes'][coding['node']] # id of coding node
+            j_graph['nodes'][coding_node_id]['coding'] = coding_string
+        # add utterance to meta. Problem: columns 'misc' and 'feats' are not stored in dict, but splitted
+        get_misc_string(graph, "1")
+
+        j_graph["meta"]["utterance"] = graph.to_sentence()
 
         print(graph.to_conll())  # Print matched sentences in CoNLL-U format
 
         sent_id = match['sent_id']
         nodes = match['matching']['nodes']
     return matched_graphs
+
+def get_misc_string(graph, node_id):
+    "get value of 'misc' column as whole string"
+    feats = ''
+    conll = graph.to_conll()
+    reNode = re.compile(rf'{node_id}\t.*\t(.*?)\n')
+    m = re.search(reNode, conll)
+    if m:
+        feats = m.group(1)
+        return feats
+    else:
+        return ''
 
 def count_graphs(conllu_file):
     graph_count = 0
@@ -90,7 +123,6 @@ def main(query_file, conllu_file, args):
     # Read and parse the GREW query
     query_content = read_grew_query(query_file)
     print(f"Query:\n{query_content}")
-    nodes = parse_grew_query(query_content)
     
     # Match sentences in the CoNLL-U file against the GREW query
     matched_sentences = match_sentences_with_query(conllu_file, query_content)

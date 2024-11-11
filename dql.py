8ee43c22643d57216c90c3f5dc974cd59a4a31a2
achim <achim.stein@ling.uni-stuttgart.de>
2024-11-11 10:18:57 +0100

    fixes

--- dql.py
+++ dql.py
@@ -11,53 +11,18 @@ match_list geht nicht.
 __author__ = "Achim Stein"
 __version__ = "0.1"
 __email__ = "achim.stein@ling.uni-stuttgart.de"
-__status__ = "10.11.24"
+__status__ = "11.11.24"
 __license__ = "GPL"
 
 import re
 import argparse
-from grewpy import Corpus, Request, Graph
+from grewpy import Corpus, Request, CorpusDraft #Graph
 
 def read_grew_query(query_file):
     # Read Grew query file.
     with open(query_file, 'r', encoding='utf-8') as f:
         query_content = f.read()
     return query_content
-
-def parse_grew_query_ALT(query_file): 
-    nodes = []
-    coding = {}
-    coding_line = ''
-    for line in query_file.split('\n'):
-        # collect nodes in list
-        reNode = re.compile('(\w+)\s*?\[.*]')
-        m = re.search(reNode, line)
-        if m:
-            nodes.append(m.group(1))  
-        # grep coding instruction from comment line (% coding...)
-        if not coding_line and re.search(r'coding', line, re.IGNORECASE):
-            coding_line = line
-    """
-    coding line:   % coding attribute=modal value=verb node=MOD addlemma=V
-    adds "modal=verb(<lemma of V>)" to (1) meta information and (2) the 'misc' column of MOD node
-    """
-    if coding_line:
-        m = re.search(r'(attr?|attribute)=(?P<value>\w+)', coding_line, re.IGNORECASE)
-        if m:
-            coding['att'] = m.group('value')
-        m = re.search(r'(val|value)=(?P<value>\w+)', coding_line, re.IGNORECASE)
-        if m:
-            coding['val'] = m.group('value')
-        m = re.search(r'(node)=(?P<value>\w+)', coding_line, re.IGNORECASE)
-        if m:
-            coding['node'] = m.group('value')  # adds coding string to 'misc' of this node
-        m = re.search(r'(add)?lemma=(?P<value>\w+)', coding_line, re.IGNORECASE)
-        if m:
-            coding['addlemma'] = m.group('value')  # adds lemma of this node
-        print(f"Coding info: {coding}")
-    else:
-        print("Coding instructions not found.  Use a comment to define them, e.g. '% coding attribute=modal value=verb node=VERB'")
-    return coding
 
 def parse_grew_query(query_file): 
     """
@@ -98,49 +63,36 @@ def find_matches(corpus, patterns):
     """Finds all sentences that match any of the queries (patterns)."""
     unique_ids = set()  # Use a set to avoid duplicates
     all_matches = []    # The matching structures
-    match_list_pattern = {}
+    matches_for_pattern = {}
     for nr in patterns.keys():
         print(f"  Searching corpus query no. {nr}...")
         request = Request.parse(patterns[nr])
         match_list = corpus.search(request)
-        print(f"    Found {len(match_list)} matches for query:\n{patterns[nr]}")
-        match_list_pattern[nr] = match_list  # store the list of matches in a dict with patter nr as key
-        for match in match_list:
+        print(f"    Found {len(match_list)} matches for query") #:\n{patterns[nr]}
+        matches_for_pattern[nr] = match_list  # store the list of matches in a dict with patter nr as key
+        for match in match_list:  # TODO not needed
             sent_id = match['sent_id']
             if sent_id not in unique_ids:  # set() allows fast membership testing
                 unique_ids.add(sent_id)  # Add the sent_id to the set
                 all_matches.append(match)  # Append the full match object to the list
     print(f"Found {len(unique_ids)} matches for all the patterns")
 #    return unique_ids, all_matches
-    return match_list_pattern
+    return matches_for_pattern
 
-def match_sentences_with_query(conllu_file, query_content):
+def match_sentences_with_query_ALT(conllu_file, query_content):
     """
-    Match each sentence against the pattern(s) of the query file.
-    Add codings if present.
+    Match each sentence against queries. Add codings if present.
     """
-    per_min = 450000  # time for loading conllu in Grew Corpus object
-    count = count_graphs(conllu_file)
-    secs = int(count / per_min * 60)
-    minutes, seconds = divmod(secs, 60)
-
-
-    # init Grew objects (initiating Corpus object is slow)
-    print(f"Estimated time to read {count} graphs: {round(minutes,0)}m {seconds}s")
-    corpus = Corpus(conllu_file)  # corpus file as Corpus object (slow)
+    corpus = init_grew_corpus(conllu_file)
     codings, patterns = parse_grew_query(query_content)
 #    unique_ids, all_matches = find_matches(corpus, patterns)
-    match_list_pattern = find_matches(corpus, patterns)
-  
+    matches_for_pattern = find_matches(corpus, patterns)
+    corpus.apply(lambda g: print_or_code(g, match_ids))
     # Iterate through each matching graph
     match_nr = 0 
     matched_graphs = []
-    """
-    ===============   TODO  alle Graphen mit .apply() behandeln.
-    add_coding  muss prüfen, ob graph in liste der sent_ids für eine Regel ist.
-    """
-    for nr in match_list_pattern.keys():
-        all_matches = match_list_pattern[nr]
+    for nr in matches_for_pattern.keys():
+        all_matches = matches_for_pattern[nr]
         for match in all_matches:
             match_nr += 1
             sent_id = match['sent_id']
@@ -148,8 +100,6 @@ def match_sentences_with_query(conllu_file, query_content):
             if match_nr > args.max:
                 print(f"Maximum match number {args.max} reached: search stopped.")
                 return matched_graphs
-    #        nodes = match['matching']['nodes'] # word IDs matching nodes in query
-    #        print(f"\n===> Match nr = {match_nr} | nodes = {nodes}\n  {match}")
             matched_graphs.append(graph)
             print(graph.to_sentence())
             j_graph = graph.json_data()  # graph in JSON format
@@ -160,12 +110,49 @@ def match_sentences_with_query(conllu_file, query_content):
             get_misc_string(graph, "1")
             j_graph["meta"]["utterance"] = graph.to_sentence()
             print(graph.to_conll())  # Print matched sentences in CoNLL-U format
-
 #        sent_id = match['sent_id']
 #        nodes = match['matching']['nodes']
     return matched_graphs
 
-def add_coding(match, j_graph, coding, pattern):
+def match_sentences_with_query(conllu_file, query_content):
+    grew_corpus = init_grew_corpus(conllu_file)
+    codings, patterns = parse_grew_query(query_content)
+#    unique_ids, all_matches = find_matches(corpus, patterns)
+    matches_for_pattern = find_matches(grew_corpus, patterns)
+    print("Make draft corpus")
+    # Convert it to a CorpusDraft if it isn't already
+    corpus = CorpusDraft(grew_corpus) if not isinstance(grew_corpus, CorpusDraft) else grew_corpus
+    print("Done.")
+    # corpus is a dict of sent_id:GraphObject
+    for sent_id, graph in corpus.items():
+        add_coding(graph, sent_id, matches_for_pattern[1])
+
+def add_coding(graph, sent_id, match_ids):
+    """
+    The Corpus itself seems to serve as a container for Graph objects, without direct access to the metadata of each graph.
+    We need to retrieve each Graph individually and then access its meta attribute using .json_data()
+    QUESTION: how can I assign modified data back to the graph ???
+    """
+    # Only apply coding if this sent_id is in the list of matches
+    j_graph = graph.json_data()
+    for match in match_ids:
+        #print(f" sent_id = {sent_id} >>>{m}")
+        if sent_id == match['sent_id']:
+            print(f" matching nodes: {match['matching']}")
+            print(f"JSON: {graph.json_data()['meta']}")
+            # Perform modifications
+            j_graph['meta']["coding"] = '' # add empty coding line
+            print(f" sent_id = {sent_id} >>>{match['matching']}\n   New Graph: {j_graph.to_conll()}")
+        
+    # Return the modified graph (or unmodified if no match)
+    return graph
+
+def get_meta(corpus, sent_id):
+    # get meta from Grew corpus object for a sentence
+    meta = corpus['meta'][sent_id]
+    return meta
+
+def add_coding_ALT(match, j_graph, coding, pattern):
     """Add coding to this sentence in meta header and in misc column of the specified node"""
     j_graph["meta"]["coding"] = '' # add empty coding line
     # add coding to meta
@@ -177,6 +164,16 @@ def add_coding(match, j_graph, coding, pattern):
     coding_node_id = match['matching']['nodes'][coding['node']] # id of coding node
     j_graph['nodes'][coding_node_id]['coding'] = coding_string
     return j_graph
+
+def init_grew_corpus(conllu_file):
+    # init Grew corpus object (this is slow: print estimate)
+    count = count_graphs(conllu_file)
+    per_min = 450000  # processed sentences per min on Mac M2
+    secs = int(count / per_min * 60)
+    minutes, seconds = divmod(secs, 60)
+    print(f"Estimated time to read {count} graphs: {round(minutes,0)}m {seconds}s")
+    corpus = Corpus(conllu_file)  # corpus file as Corpus object (slow)
+    return corpus
 
 def get_misc_string(graph, node_id):
     "get value of 'misc' column as whole string"
@@ -191,15 +188,16 @@ def get_misc_string(graph, node_id):
         return ''
 
 def count_graphs(conllu_file):
+    # count empty lines as end of graph
     graph_count = 0
     with open(conllu_file, 'r', encoding='utf-8') as file:
         for line in file:
-            if line.strip() == '':  # Each empty line indicates the end of a graph
+            if line.strip() == '':
                 graph_count += 1
     return graph_count
 
 def main(query_file, conllu_file, args):
-    # Read and parse the GREW query
+    # Read and parse the Grew query
     query_content = read_grew_query(query_file)
     print(f"Query:\n{query_content}")
     
